f790afbf89c59e67185dabe9ccdd18c71cee8650
Soul-cp <izzatkhilfi@gmail.com>
2025-04-21 15:47:30 +0800

    refactor: update package dependencies and improve app initialization

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,17 +5,31 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.vue`,
+            import.meta.glob('./Pages/**/*.vue')
+        ),
     setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
-            .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+        const app = createApp({ render: () => h(App, props) });
+
+        app.use(plugin);
+        app.use(ZiggyVue);
+        app.use(PrimeVue, {
+            theme: {
+                preset: Aura,
+            },
+        });
+
+        app.mount(el);
+        return app;
     },
     progress: {
         color: '#4B5563',
