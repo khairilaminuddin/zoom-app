c6c7bccc4927d9ec6f68ee2b40358a7ea16c09b6
Rasyidi-Office <rasyidialwee@gmail.com>
2025-04-22 11:44:17 +0800

    refactor: update DatabaseSeeder and UserSeeder to use User factory for seeding

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,36 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $users = [
+            [
+                'name' => 'Rasyidi Alwee',
+                'email' => 'rasyidi@gmail.com',
+            ],
+            [
+                'name' => 'Hazwan Syahmi',
+                'email' => 'hazsyah02@gmail.com',
+            ],
+            [
+                'name' => 'Irfan Izzat Khilfi',
+                'email' => 'nikarsyad07@gmail.com',
+            ],
+            [
+                'name' => 'Haziq Zulkifli',
+                'email' => 'haziqzulkifl1020@gmail.com',
+            ],
+            [
+                'name' => 'Arsyad',
+                'email' => 'irfanijatron@gmail.com',
+            ],
+            [
+                'name' => 'Khairil',
+                'email' => 'khairil1721@gmail.com',
+            ],
+
+        ];
+
+        foreach ($users as $user) {
+            User::factory()->create($user);
+        }
     }
 }
