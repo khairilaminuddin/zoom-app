d8905e15e12f8bfc69f5f21b87bd1f27c07c4398
khairilaminuddin <khairil1721@gmail.com>
2025-04-24 15:34:03 +0800

    repair  untuk bahagian table zooms

--- database/factories/ZoomFactory.php
+++ database/factories/ZoomFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
+use App\Models\Zoom;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(class: [
-            UserSeeder::class,
             ZoomSeeder::class,
         ]);
     }
